cdfed1e0a18f26bc6baf75a5354f48b75e4b6726
Pihla Jarv <pihla.jarv@gmail.com>
2024-05-09 00:42:07 +0300

    Revert "parandasin vea equals meetodis"
    This reverts commit 84148973dcfe86b0310a363863af588b224a93bb.
    
    olin meetodi signatuuri muutnud, nii et oleks pidanud teiste failide muudatused ka panema

--- src/main/massiivioperatsioonid/TööalaValimineVõiMuutmine.java
+++ src/main/massiivioperatsioonid/TööalaValimineVõiMuutmine.java
@@ -6,7 +6,7 @@ import main.MassiiviSeis;
 import java.util.Objects;
 
 public abstract class TööalaValimineVõiMuutmine extends Massiivioperatsioon {
-    public TööalaValimineVõiMuutmine(int uusTööalaLõpp, int uusTööalaAlgus, MassiiviSeis massiivEnneOperatsiooni) {
+    public TööalaValimineVõiMuutmine(MassiiviSeis massiivEnneOperatsiooni, int uusTööalaAlgus, int uusTööalaLõpp) {
         super(massiivEnneOperatsiooni);
         this.massiivPealeOperatsiooni.setTööalaAlgusIndeks(uusTööalaAlgus);
         this.massiivPealeOperatsiooni.setTööalaleJärgnevIndeks(uusTööalaLõpp);
@@ -23,11 +23,11 @@ public abstract class TööalaValimineVõiMuutmine extends Massiivioperatsioon {
         }
 
         return Objects.equals(this.massiivPealeOperatsiooni.getTööalaAlgusIndeks(), tööalaValimineVõiMuutmine.massiivPealeOperatsiooni.getTööalaAlgusIndeks())
-                && Objects.equals(this.massiivPealeOperatsiooni.getTööalaleJärgnevIndeks(), tööalaValimineVõiMuutmine.massiivPealeOperatsiooni.getTööalaleJärgnevIndeks());
+                && Objects.equals(this.massiivPealeOperatsiooni.getTööalaleJärgnevIndeks(), getMassiivPealeOperatsiooni().getTööalaleJärgnevIndeks());
     }
 
     @Override
     public String toString() {
-        return "Tööala valimine "+ massiivPealeOperatsiooni.getTööalaAlgusIndeks() + ", " + massiivPealeOperatsiooni.getTööalaleJärgnevIndeks()+". Uus tööala: " + this.massiivPealeOperatsiooni;
+        return "Tööala valimine. Uus tööala: " + this.massiivPealeOperatsiooni;
     }
 }
